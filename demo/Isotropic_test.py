56021d7c666cd583abb68d89088b47eb0b3e2a70
Martin Boden <mboden@ethz.ch>
2021-02-11 18:17:33 +0100

    cleaned script

--- demo/Isotropic_test.py
+++ demo/Isotropic_test.py
@@ -262,8 +262,8 @@ def update(context):
             w.append(dissipation)
 
             if params.tstep % (params.compute_energy*10) == 0:
-                print(' Tstep   Time       Energy  eps_forcing   eps_l2vort      eps_l2J      eps_rhs     eps_dEdt    Re_dissip   Re_forcing')            
-            print('{tstep:6d} {t:.4f} {e_current:12.4f} {eps_forcing:12.4f} {eps_l2vort:12.4f} {eps_l2J:12.4f} {eps_rhs:12.4f} {eps_dEdt:12.4f} {Re_lam_eps_dissipation:12.4f} {Re_lam_eps_forcing:12.4f}'.format(
+                print(' Tstep    Time  Energy  eps_forcing   eps_l2vort      eps_l2J      eps_rhs     eps_dEdt    Re_dissip   Re_forcing')            
+            print('{tstep:6d} {t:7.4f} {e_current:7.4f} {eps_forcing:12.4f} {eps_l2vort:12.4f} {eps_l2J:12.4f} {eps_rhs:12.4f} {eps_dEdt:12.4f} {Re_lam_eps_dissipation:12.4f} {Re_lam_eps_forcing:12.4f}'.format(
                     tstep=params.tstep,t=params.t, e_current=e_current, eps_forcing=eps_forcing, eps_l2vort=eps_l2vort, 
                     eps_l2J=eps_l2J, eps_rhs=eps_rhs, eps_dEdt=eps_dEdt, 
                     Re_lam_eps_dissipation=Re_lam_eps_dissipation, Re_lam_eps_forcing=Re_lam_eps_forcing),flush=True)
@@ -359,7 +359,7 @@ if __name__ == "__main__":
 
         print('Running simulations:')
 
-        print(' Tstep   Time       Energy  eps_forcing   eps_l2vort      eps_l2J      eps_rhs     eps_dEdt    Re_dissip   Re_forcing')            
+        print(' Tstep   Time       Energy  eps_forcing   eps_l2vort      eps_l2J      eps_rhs     eps_dEdt    Re_dissip   Re_forcing',flush=True)            
 
     solve(solver, context)
 
