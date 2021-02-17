01cf80b85c188b3ffe0114a90bc621615ccf53d8
mboden <mboden@ethz.ch>
2021-02-17 14:50:21 +0100

    changes

--- runs/Isotropic_test.py
+++ runs/Isotropic_test.py
@@ -322,8 +322,8 @@ if __name__ == "__main__":
     config.params.U_k = U_k
 
     config.params.dt = T_k/config.params.N[0] # Set time step to 1/N the kolmogorov time step
-    config.params.compute_energy = 32   # Compute energy every komlmogorv time scale
-    config.params.compute_spectrum = 32 # Compute spectrum every komlmogorv time scale
+    config.params.compute_energy = 64   # Compute energy every komlmogorv time scale
+    config.params.compute_spectrum = 64 # Compute spectrum every komlmogorv time scale
 
     # Initialize turbulence
     initialize(solver, context)
