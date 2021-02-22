3b45bb1f7f40c5efa86a0c5726633d7c4f1c883e
Martin Boden <mboden@ethz.ch>
2021-02-22 22:27:19 +0100

    minor greasy

--- deep-les/launch_scripts/runDNS_transient.sh
+++ deep-les/launch_scripts/runDNS_transient.sh
@@ -9,9 +9,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=normal
 #SBATCH --constraint=gpu
-#SBATCH --job-name=KSGP64L22_0_greasy_autoencoder_hyperparam_study
-#SBATCH --output=/scratch/snx3000/mboden/spectralDNS/deep-les/results/DNS_transient/_logs/DNS_transient_out_JID%j_A%a.txt
-#SBATCH --error=/scratch/snx3000/mboden/spectralDNS/deep-les/results/DNS_transient/_logs/DNS_transient_err_JID%j_A%a.txt
+#SBATCH --job-name=DNStransient
 
 # ======START===== #
 
