72b09920f0ec64bfafad0ac39ee88573b0697ca5
Martin Boden <mboden@ethz.ch>
2021-02-22 20:46:15 +0100

    test greeay

---
+++ deep-les/launch_scripts/runDNS_test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+#
+#SBATCH --account=s929
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=mboden@ethz.ch
+#SBATCH --time=1:00:00
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=12
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=normal
+#SBATCH --constraint=gpu
+#SBATCH --job-name=DNStransient_test
+#SBATCH --output=/scratch/snx3000/mboden/spectralDNS/deep-les/results/DNS_transient/_logs/DNS_transient_out_JID%j_A%a.txt
+#SBATCH --error=/scratch/snx3000/mboden/spectralDNS/deep-les/results/DNS_transient/_logs/DNS_transient_err_JID%j_A%a.txt
+
+# ======START===== #
+
+module load daint-gpu cray-fftw h5py
+source ~/myvenv-spectralDNS/bin/activate
+greasy ./greasy_tasks/runDNStransient.txt
