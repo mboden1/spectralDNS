d2f96701081d1181bff5ae46c8cafbd407172ab2
Martin Boden <mboden@ethz.ch>
2021-02-22 20:07:28 +0100

    test

--- deep-les/spectralDNS/Isotropic_transient.py
+++ deep-les/spectralDNS/Isotropic_transient.py
@@ -318,7 +318,7 @@ if __name__ == "__main__":
     solver = get_solver(update=update, mesh="triplyperiodic")
     context = solver.get_context()
 
-    data_path = '../results/DNS_transient/DNS_N{}_Re{}_dtr{}_{}/'.format(config.params.N,
+    data_path = '../results/DNS_transient/DNS_N{}_Re{}_dtr{}_{}/'.format(config.params.N[0],
                                                                     config.params.Re_lam,
                                                                     str(config.params.run).zfill(2))
     os.mkdir(data_path)
