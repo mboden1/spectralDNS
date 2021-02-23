eef5a04ebd8b1b6085162f070c3c4424bf86ec4c
Martin Boden <mboden@ethz.ch>
2021-02-23 16:45:30 +0100

    minor single

--- deep-les/launch_scripts/runDNS_single.sh
+++ deep-les/launch_scripts/runDNS_single.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=mboden@ethz.ch
 #SBATCH --time=20:00:00
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=12
 #SBATCH --partition=normal
@@ -17,4 +17,4 @@ module load daint-gpu cray-fftw h5py GREASY
 source ~/myvenv-spectralDNS/bin/activate
 
 cd /scratch/snx3000/mboden/spectralDNS/deep-les/spectralDNS/
-srun -n 24 python3 Isotropic_transient.py --N 256 256 256 --forcing_mode constant_eps --init_mode Lamorgese --save_path ../results/DNS_single/ --Re_lam 60 --dt_ratio 2 --run 1 NS
+srun -n 12 python3 Isotropic_transient_temp.py --N 256 256 256 --forcing_mode constant_eps --init_mode Lamorgese --save_path ../results/DNS_single/ --Re_lam 60 --dt_ratio 2 --ratio_print 64 --ratio_save 64 --run 1 NS
